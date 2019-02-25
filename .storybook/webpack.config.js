a8134ade6dcc9bf18637efcc6fc489110c0f5e4c
klimentru1986 <kliment1986@inbox.ru>
2019-02-25 22:34:32 +0300

    css loader error fix

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -7,13 +7,17 @@ module.exports = baseConfig => {
       {
         loader: require.resolve('@storybook/addon-storysource/loader'),
         options: {
-          parser: 'typescript',
-        },
-      },
+          parser: 'typescript'
+        }
+      }
     ],
     include: [path.resolve(__dirname, '../src')],
-    enforce: 'pre',
+    enforce: 'pre'
   });
 
+  baseConfig.module.rules.push({
+    test: /\.css$/,
+    use: ['to-string-loader', 'css-loader']
+  });
   return baseConfig;
 };
